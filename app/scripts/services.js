8ac75aab835bd4485d0d0eee9c75c4994ba62d6e
laura-dimitra <laura@integrationpartners.nl>
2019-04-25 17:24:21 +0200

    Clean up code

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,101 +1,85 @@
-(function(){
+(function () {
 
-'use strict';
+    'use strict';
     var app = angular.module('claimapp');
 
     app.constant('API_URL', "http://localhost:80/ija_tibet2/api");
-    app.factory('StaticDataFactory', function($http, API_URL)
-    {
-        
+    app.factory('StaticDataFactory', function ($http, API_URL) {
 
-        return{
-            getAllTibcoQueues : getAllTibcoQueues,
-            claimQueue : claimQueue,
-            unClaimQueue :unClaimQueue,
-            GetClaimRoleList : GetClaimRoleList, 
-            GetClaimedRecordsList : GetClaimedRecordsList
+
+        return {
+            getAllTibcoQueues: getAllTibcoQueues,
+            claimQueue: claimQueue,
+            unClaimQueue: unClaimQueue,
+            GetClaimRoleList: GetClaimRoleList,
+            GetClaimedRecordsList: GetClaimedRecordsList
         };
 
-        function claimQueue(businessdomain, servapplname, role, claimrole)
-        {
-            var xmlstring = "<input><businessdomain>" + businessdomain + "</businessdomain><servapplname>" + servapplname + "</servapplname><claimrole>" +  claimrole + "</claimrole><role>" + role + "</role></input>"
-            return $http({method: 'POST',url: API_URL + '/claimqueue', data : xmlstring, headers: {"Content-Type" : 'application/xml'}})
-            .then(function success(response)
-            {
-                return response;
-            },function failure(failmessage)
-            {
-            	return failmessage;
-            });
+        function claimQueue(businessdomain, servapplname, role, claimrole) {
+            var xmlstring = "<input><businessdomain>" + businessdomain + "</businessdomain><servapplname>" + servapplname + "</servapplname><claimrole>" + claimrole + "</claimrole><role>" + role + "</role></input>"
+            return $http({ method: 'POST', url: API_URL + '/claimqueue', data: xmlstring, headers: { "Content-Type": 'application/xml' } })
+                .then(function success(response) {
+                    return response;
+
+                }, function failure(failmessage) {
+                    return failmessage;
+                });
         }
 
-        function unClaimQueue(businessdomain, servapplname,  role, claimrole,)
-        {
-            var xmlstring = "<input><businessdomain>" + businessdomain + "</businessdomain><servapplname>" + servapplname + "</servapplname><claimrole>" +  claimrole + "</claimrole><role>" + role + "</role></input>"
+        function unClaimQueue(businessdomain, servapplname, role, claimrole, ) {
+            var xmlstring = "<input><businessdomain>" + businessdomain + "</businessdomain><servapplname>" + servapplname + "</servapplname><claimrole>" + claimrole + "</claimrole><role>" + role + "</role></input>"
             console.log(xmlstring);
-            return $http({method: 'POST',url: API_URL + '/unclaimapplications', data : xmlstring, headers: {"Content-Type" : 'application/xml'}})
-            .then(function success(response)
-            {
-            	console.log('reply from server:' , response.data);
-                return response;
-            },function failure(failmessage)
-            {
-            	console.log('fail from server:', failmessage.data);
-                return failmessage;
-            });
+            return $http({ method: 'POST', url: API_URL + '/unclaimapplications', data: xmlstring, headers: { "Content-Type": 'application/xml' } })
+                .then(function success(response) {
+                    console.log('reply from server:', response.data);
+                    return response;
+                }, function failure(failmessage) {
+                    console.log('fail from server:', failmessage.data);
+                    return failmessage;
+                });
         }
 
-        function getAllTibcoQueues()
-        {
-          return $http.get(API_URL + '/getalltibcoqueues').then(function(data)
-            {
+        function getAllTibcoQueues() {
+            return $http.get(API_URL + '/getalltibcoqueues').then(function (data) {
                 var afterCnv = xml.xmlToJSON(data.data)
 
-                console.info("returning json from server with status ",afterCnv);
+                console.info("returning json from server with status ", afterCnv);
                 return afterCnv;
 
-            },function (error)
-            {
-              console.log("server error :", error );
+            }, function (error) {
+                console.log("server error :", error);
             });
         }
 
-        function GetClaimRoleList()
-        {
-          return $http.get(API_URL + '/getclaimrolelist').then(function(data)
-            {
+        function GetClaimRoleList() {
+            return $http.get(API_URL + '/getclaimrolelist').then(function (data) {
                 var afterCnv = xml.xmlToJSON(data.data)
 
                 console.log("functie get claimrolelist ophalen", afterCnv);
                 return afterCnv;
 
-            },function (error)
-            {
-              console.log("server error :", error );
-              return error;
+            }, function (error) {
+                console.log("server error :", error);
+                return error;
             });
         }
 
-        function GetClaimedRecordsList()
-        {
-            return $http.get(API_URL + '/getclaimedrecords').then(function(data)
-            {
-                var afterCnv = xml.xmlToJSON(data.data) 
-                afterCnv.claimedrecords.record.splice(0,1);
-                
-                afterCnv.claimedrecords.record.forEach( function(element)
-                {
-                    element.queue.splice(0,1);   
+        function GetClaimedRecordsList() {
+            return $http.get(API_URL + '/getclaimedrecords').then(function (data) {
+                var afterCnv = xml.xmlToJSON(data.data)
+                afterCnv.claimedrecords.record.splice(0, 1);
+                console.log("claimedrecordlist na splice", afterCnv.claimedrecords.record.splice(0, 1));
+                afterCnv.claimedrecords.record.forEach(function (element) {
+                    element.queue.splice(0, 1);
                 });
-                
+
                 console.log("alle claimed records na conversie naar json", afterCnv.claimedrecords.record);
                 return afterCnv;
-            
 
-            },function (error)
-            {
-              console.log("server error :", error );
-              return error;
+
+            }, function (error) {
+                console.log("server error :", error);
+                return error;
             });
         }
 
